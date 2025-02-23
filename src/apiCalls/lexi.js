7c0c60bdc76875b8bef41568273196cddb0b3bec
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-02-23 10:48:58 +0900

    chore: update token

--- src/apiCalls/lexi.js
+++ src/apiCalls/lexi.js
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 export const getLexiChat = async (tickerSymbol) => {
   const client = new OpenAI({
     baseURL: "https://venalisono.ap.xpressai.cloud/api/financials/",
-    apiKey: "6c732b41-67fe-4475-9a7c-e71785aa38df",
+    apiKey: "715e5a8-6650-4921-acc7-ad9ff3efa153",
   });
 
   const response = await client.chat.completions.create({
