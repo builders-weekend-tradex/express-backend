56831130f1813ba38f62fea786da84220391277b
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-02-23 16:54:27 +0900

    go

--- src/apiCalls/lexi.js
+++ src/apiCalls/lexi.js
@@ -18,3 +18,22 @@ export const getLexiChat = async (tickerSymbol) => {
 
   return response;
 };
+
+export const getMoreInformation = async (tickerSymbol) => {
+  const client = new OpenAI({
+    baseURL: "https://venalisono.ap.xpressai.cloud/api/fin/",
+    apiKey: "7acaa0cf-43f3-4480-a67b-655244ee1915",
+  });
+
+  const response = await client.chat.completions.create({
+    model: "Lexi", // Available models: Lexi, LexiOnboarding
+    messages: [
+      {
+        role: "user",
+        content: tickerSymbol,
+      },
+    ],
+  });
+
+  return response;
+};
