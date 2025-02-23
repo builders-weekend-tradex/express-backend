85c0c926a4fae79ea47e632a2e8deb12a5c21bb0
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-02-23 10:06:25 +0900

    feat: update endpoint

--- src/apiCalls/lexi.js
+++ src/apiCalls/lexi.js
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 export const getLexiChat = async (tickerSymbol) => {
   const client = new OpenAI({
     baseURL: "https://venalisono.ap.xpressai.cloud/api/financials/",
-    apiKey: "b6750e38-5ea3-4452-b854-e27977533c23",
+    apiKey: "6c732b41-67fe-4475-9a7c-e71785aa38df",
   });
 
   const response = await client.chat.completions.create({
