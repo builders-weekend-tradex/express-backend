ce3452d3b724c4306213d4ca9ac76a307d98cb92
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-02-23 15:21:32 +0900

    fix: add security

--- src/server.js
+++ src/server.js
@@ -71,6 +71,9 @@ app.post("/email", async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.USER_NM,
         pass: process.env.PASSWORD_NM,
